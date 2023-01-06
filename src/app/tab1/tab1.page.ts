dc7426c60cf0f8f95a814f42d8d4183440375a4c
petrrw <>
2023-01-06 19:22:04 +0100

    Add splash screen

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,6 +3,8 @@ import { debounceTime, distinctUntilChanged, Observable, Subject } from 'rxjs';
 import { PokemonApiService } from '../services/pokemon-api.service';
 import { of } from 'rxjs';
 
+import { SplashScreen } from '@capacitor/splash-screen';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -12,6 +14,10 @@ export class Tab1Page {
   public showOnlyFavorites: boolean = false
  
   constructor(private pokemonApi : PokemonApiService) {
+    SplashScreen.show({
+      showDuration: 2000,
+      autoHide: true,
+    });
     this.pokemons = pokemonApi.getPokemons(50);
     console.log(this.pokemons)
   }
@@ -20,9 +26,6 @@ export class Tab1Page {
   pokemons: Observable<any[]>
 
   ngOnInit() : void {
-   
-
-    
   }
 
 }
